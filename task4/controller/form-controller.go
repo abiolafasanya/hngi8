28e2258ce5eac26c400a8a84e9b2979bf770e581
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-08-17 22:13:53 +0100

    commit

--- task4/controller/form-controller.go
+++ task4/controller/form-controller.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"net/http"
 
@@ -11,6 +13,15 @@ import (
 var tmpl = template.Must(template.ParseFiles("view/index.html"))
 
 func Contact(w http.ResponseWriter, r *http.Request) {
+
+	// Connect database
+	db, err := sql.Open("mysql", "b7ca0ef21f6a3e:4c49d0c9@tcp(s-cdbr-east-04.cleardb.com)/heroku_da5272d9c3c8ace?")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	fmt.Println("Database connected successfullly")
+
 	if r.Method != http.MethodPost {
 		tmpl.Execute(w, nil)
 		return
@@ -22,5 +33,19 @@ func Contact(w http.ResponseWriter, r *http.Request) {
 		Message: r.FormValue("message"),
 	}
 	_ = data
+	//insert message database contact
+	newContact := new(models.Contact)
+	newContact.Name = r.FormValue("name")
+	newContact.Email = r.FormValue("email")
+	newContact.Subject = r.FormValue("subject")
+	newContact.Message = r.FormValue("message")
+
+	insert, err := db.Query("INSERT INTO users (firstname, lastname, email) VALUES ('abiola', 'fasanya', 'abiola@mail.com')")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer insert.Close()
+	fmt.Println("Data Inserted Successfullly")
+	//mysql: //b7ca0ef21f6a3e:4c49d0c9@us-cdbr-east-04.cleardb.com/heroku_da5272d9c3c8ace?
 	tmpl.Execute(w, struct{ Success bool }{true})
 }
