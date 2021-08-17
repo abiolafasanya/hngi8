be0cab14eef27f03404149a791a09f7d530ab65c
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-08-17 21:13:44 +0100

    commit

--- task4/server.go
+++ task4/server.go
@@ -3,18 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/hngi8/task4/controller"
 )
 
 func main() {
 
-	http.FileServer(http.Dir("public/css"))
-	// http.Handle("/public/css", http.StripPrefix("/public/css/style.css", fs))
+	PORT := os.Getenv("PORT")
+	if PORT == ""  {
+		PORT = "3000"
+	}
+	// http.FileServer(http.Dir("public/css"))
+	// // http.Handle("/public/css", http.StripPrefix("/public/css/style.css", fs))
 
 	http.HandleFunc("/", controller.Contact)
 
 
-	fmt.Println("Server Started....")
-	http.ListenAndServe(":3000", nil)
+	fmt.Println("Server Started on port"+PORT+"....")
+	http.ListenAndServe(":"+PORT, nil)
 }
