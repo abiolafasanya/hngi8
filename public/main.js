55fd02f5fb439b56fd4cee0f2ea70eac61b5d9fc
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-08-26 00:02:44 +0100

    commit

--- public/main.js
+++ public/main.js
@@ -7,3 +7,35 @@
  setTimeout(() => {
   msgAlert.classList.add("hidden");
  }, 6000)
+
+ let skillSection = document.querySelector('#skills-section')
+ let progressBar = document.querySelectorAll('.skill-progress');
+
+ const showProgressAnimation = () => {
+   progressBar.forEach(progress => {
+      progress.classList.add('skill-animation')
+   })
+ }
+
+ const hideProgressAnimation = () => {
+   progressBar.forEach(progress => {
+      progress.classList.remove('skill-animation')
+   })
+ }
+
+
+ window.addEventListener('scroll', () => {
+   const sectionPos = skillSection.getBoundingClientRect().top
+   const screenPos = window.innerHeight
+
+   if(sectionPos < screenPos){
+      console.log('show progress bar')
+      showProgressAnimation()
+   }
+   else {
+      console.log('hide progress bar')
+      hideProgressAnimation()
+   }
+ })
+
+ 
